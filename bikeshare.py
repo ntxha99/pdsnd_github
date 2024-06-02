ab9716b481e73848e6f1dbc2ae34bc6246ea6cd3
Nhan <hdtnhan91@gmail.com>
2024-06-02 19:57:04 +0700

    refactoring: edit condition at main in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -172,7 +172,7 @@ def main():
         user_stats(df)
         display_data(df)
         restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        if restart.lower() == 'no':
             break
 
 
